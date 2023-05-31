b5abc1e7872557a014c516200816ccf99d8a427e
Becky1703 <oluwabukunmia@gmail.com>
2023-05-31 05:10:44 -0700

    fixed error

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,16 +14,9 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 if (node == NULL || node->parent == NULL)
-{
 return (NULL);
-}
-binary_tree_t *parent = node->parent;
-if (parent->left == node)
-{
-return (parent->right);
-}
-else
-{
-return (parent->left);
-}
+
+if (node->parent->left == node)
+return (node->parent->right);
+return (node->parent->left);
 }
